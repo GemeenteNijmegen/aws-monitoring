5493166b3451b73962b4984f7057b3c404700fdf
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-04 17:09:06 +0200

    lint

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { PipelineStack } from './PipelineStack';
 
 // for development, use sandbox account
 const deploymentEnvironment = {
-	account: '418648875085',
-	region: 'eu-west-1',
+  account: '418648875085',
+  region: 'eu-west-1',
 };
 
 const sandboxEnvironment = {
-	account: '122467643252',
-	region: 'eu-west-1',
+  account: '122467643252',
+  region: 'eu-west-1',
 };
 
 // const acceptanceEnvironment = {
@@ -24,47 +24,47 @@ const sandboxEnvironment = {
 
 const app = new App();
 if ('BRANCH_NAME' in process.env == false || process.env.BRANCH_NAME == 'development') {
-	new PipelineStack(app, 'aws-monitoring-pipeline-development',
-		{
-			env: deploymentEnvironment,
-			branchName: 'development',
-			deployToEnvironment: sandboxEnvironment,
-			environmentName: 'sandbox'
-		},
-	);
+  new PipelineStack(app, 'aws-monitoring-pipeline-development',
+    {
+      env: deploymentEnvironment,
+      branchName: 'development',
+      deployToEnvironment: sandboxEnvironment,
+      environmentName: 'sandbox',
+    },
+  );
 }
 
 
 const deploymentEnvironments = [
-	{ 
-		name: 'auth-accp',
-		env: {
-			account: '315037222840',
-			region: 'eu-west-1',
-		}
-	}
-]
+  {
+    name: 'auth-accp',
+    env: {
+      account: '315037222840',
+      region: 'eu-west-1',
+    },
+  },
+];
 
 if ('BRANCH_NAME' in process.env == false || process.env.BRANCH_NAME == 'main') {
-	new PipelineStack(app, 'aws-monitoring-pipeline-development',
-		{
-			env: deploymentEnvironment,
-			branchName: 'development',
-			deployToEnvironment: sandboxEnvironment,
-			environmentName: 'sandbox'
-		},
-	);
+  new PipelineStack(app, 'aws-monitoring-pipeline-development',
+    {
+      env: deploymentEnvironment,
+      branchName: 'development',
+      deployToEnvironment: sandboxEnvironment,
+      environmentName: 'sandbox',
+    },
+  );
 } else if ( process.env.BRANCH_NAME == 'main') {
-	deploymentEnvironments.forEach(environment => {
-		new PipelineStack(app, `aws-monitoring-pipeline-${environment.name}`,
-		{
-			env: deploymentEnvironment,
-			branchName: 'main',
-			deployToEnvironment: environment.env,
-			environmentName: environment.name
-		},
-	);
-	})
+  deploymentEnvironments.forEach(environment => {
+    new PipelineStack(app, `aws-monitoring-pipeline-${environment.name}`,
+      {
+        env: deploymentEnvironment,
+        branchName: 'main',
+        deployToEnvironment: environment.env,
+        environmentName: environment.name,
+      },
+    );
+  });
 }
 
 app.synth();
