d56679d816249fda84b29c6a291c668ed938f772
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-09 14:04:37 +0200

    Setup permissions for Eventbridge to publish to sns

--- src/MonitoringTargetStage.ts
+++ src/MonitoringTargetStage.ts
@@ -8,6 +8,7 @@ import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { NagSuppressions } from 'cdk-nag';
 import { Construct } from 'constructs';
 import { Statics } from './statics';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export interface MonitoringTargetStageProps extends StageProps {
   deployToEnvironments: { name: string; env: Environment }[];
@@ -15,10 +16,10 @@ export interface MonitoringTargetStageProps extends StageProps {
 
 export class MonitoringTargetStage extends Stage {
   /**
-	 * The monitoring target stage creates a stack containing
-	 * a monitoring topic, which can be used account-wide as a target
-	 * for monitoring notifications.
-	 */
+   * The monitoring target stage creates a stack containing
+   * a monitoring topic, which can be used account-wide as a target
+   * for monitoring notifications.
+   */
   constructor(scope: Construct, id: string, props: MonitoringTargetStageProps) {
     super(scope, id, props);
     Tags.of(this).add('cdkManaged', 'yes');
@@ -31,12 +32,12 @@ export class MonitoringTargetStage extends Stage {
 
 export class MonitoringTargetStack extends Stack {
   /**
-	 * Create a new stack containing all resources required for the monitoring
-	 * stage.
-	 *
-	 * The monitoring topic has a lambda subscriber responsible for
-	 * formatting and sending relevant notifications to a slack channel.
-	 */
+   * Create a new stack containing all resources required for the monitoring
+   * stage.
+   *
+   * The monitoring topic has a lambda subscriber responsible for
+   * formatting and sending relevant notifications to a slack channel.
+   */
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
@@ -45,8 +46,10 @@ export class MonitoringTargetStack extends Stack {
 
     const key = this.kmskey();
     const topic = this.topic(key);
+    new AlarmRuleSubscription(this, 'alarm-rule', { topic, topicKey: key });
 
-    this.subscribeToAccountwideAlarmEvents(topic);
+    
+
     this.AddLambdaSubscriber(topic);
 
     this.suppressNagging();
@@ -58,14 +61,22 @@ export class MonitoringTargetStack extends Stack {
       description: 'encryption key for monitoring stack for this account',
       alias: 'account/kms/monitoring-key',
     });
+
+    //Grant access to eventbridge for publishing to SNS
+    key.grant(new ServicePrincipal('events.amazonaws.com'),
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*"
+    );
     return key;
   }
 
   /**
-	 * Create an SNS topic and a parameter exporting the topic arn.
-	 *
-	 * @returns aws_sns.Topic the newly created topic
-	 */
+   * Create an SNS topic and a parameter exporting the topic arn.
+   *
+   * @returns aws_sns.Topic the newly created topic
+   */
   topic(key: aws_kms.Key) {
     const topic = new aws_sns.Topic(this, 'sns-topic', {
       displayName: 'Account-wide monitoring topic',
@@ -79,27 +90,16 @@ export class MonitoringTargetStack extends Stack {
   }
 
   /**
-	 * Creates a new lambda function and subscribes it to a topic.
-	 *
-	 * This lambda is responsible for formatting events posted to SNS
-	 * and sending notifications to a slack webhook.
-	 *
-	 * @param topic the SNS topic the lambda should be subscribed to
-	 */
+   * Creates a new lambda function and subscribes it to a topic.
+   *
+   * This lambda is responsible for formatting events posted to SNS
+   * and sending notifications to a slack webhook.
+   *
+   * @param topic the SNS topic the lambda should be subscribed to
+   */
   AddLambdaSubscriber(topic: aws_sns.Topic) {
     const monitoringLambda = new MonitoringLambda(this, 'log-lambda');
     topic.addSubscription(new LambdaSubscription(monitoringLambda.function));
-  }
-
-  private subscribeToAccountwideAlarmEvents(topic: aws_sns.Topic) {
-    new Rule(this, 'alarm-state-changed', {
-      description: 'Send all alarm state change notifications to SNS',
-      targets: [new aws_events_targets.SnsTopic(topic)],
-      eventPattern: {
-        source: ['aws.cloudwatch'],
-        detailType: ['CloudWatch Alarm State Change'],
-      },
-    });
   }
 
   private suppressNagging() {
@@ -116,21 +116,48 @@ export class MonitoringTargetStack extends Stack {
     );
 
     /**
-		 * Deze is eigenlijk te breed, accepteert nu alle resources met wildcard in de stack. Voor zover ik kan vinden is
-		 * het niet specifieker in te richten. Ander nadeel van deze suppression is dat het op alle resources met wildcard
-		 * dit als metadata toevoegt.
-		 */
+     * Deze is eigenlijk te breed, accepteert nu alle resources met wildcard in de stack. Voor zover ik kan vinden is
+     * het niet specifieker in te richten. Ander nadeel van deze suppression is dat het op alle resources met wildcard
+     * dit als metadata toevoegt.
+     */
     NagSuppressions.addResourceSuppressions(
       this,
       [
         {
           id: 'AwsSolutions-IAM5',
-          reason: 'Setting a log retention policy on a lambda creates a log group via a custom resource, which has a wildcard permission for creating log groups.',
+          reason: 'Setting a log retention policy requires wildcard resource permission for creating log groups.',
           appliesTo: ['Resource::*'],
         },
       ],
       true,
     );
+  }
+}
+
+interface AlarmRuleSubscriptionProps {
+  topic: aws_sns.Topic;
+  topicKey: aws_kms.Key;
+}
+
+class AlarmRuleSubscription extends Construct {
+  principal: ServicePrincipal;
+
+  constructor(scope: Construct, id: string, props: AlarmRuleSubscriptionProps) {
+    super(scope, id);
+    const alarmRule = new Rule(this, 'alarm-state-changed', {
+      description: 'Send all alarm state change notifications to SNS',
+      targets: [new aws_events_targets.SnsTopic(props.topic)],
+      eventPattern: {
+        source: ['aws.cloudwatch'],
+        detailType: ['CloudWatch Alarm State Change'],
+      },
+    });
+
+    this.principal = new ServicePrincipal('events.amazonaws.com', {
+      conditions: {"StringEquals": { "sns:Endpoint": alarmRule.ruleArn }}
+    });
+
+    props.topic.grantPublish(this.principal);
   }
 }
 
