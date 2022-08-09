96efd9311abe31a45de04e9b35f5c4d5d5767f2e
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-09 10:24:50 +0200

    Add cdk-nag, suppress 2 errors

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,6 @@
-import { App } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { App, Aspects, Stack } from 'aws-cdk-lib';
+import { Annotations, Match, Template } from 'aws-cdk-lib/assertions';
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { MonitoringTargetStack } from '../src/MonitoringTargetStage';
 
 test('Snapshot', () => {
@@ -8,4 +9,35 @@ test('Snapshot', () => {
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
+});
+
+describe('cdk-nag AwsSolutions Pack', () => {
+  let app: App;
+  let stack: Stack;
+  beforeAll(() => {
+    // GIVEN
+    app = new App();
+    stack = new MonitoringTargetStack(app, 'test');
+    Aspects.of(stack).add(new AwsSolutionsChecks({ verbose: true }));
+
+    // WHEN
+    Aspects.of(stack).add(new AwsSolutionsChecks());
+  });
+
+  // THEN
+  test('No unsuppressed Warnings', () => {
+    const warnings = Annotations.fromStack(stack).findWarning(
+      '*',
+      Match.stringLikeRegexp('AwsSolutions-.*'),
+    );
+    expect(warnings).toHaveLength(0);
+  });
+
+  test('No unsuppressed Errors', () => {
+    const errors = Annotations.fromStack(stack).findError(
+      '*',
+      Match.stringLikeRegexp('AwsSolutions-.*'),
+    );
+    expect(errors).toHaveLength(0);
+  });
 });
