a68b4dcb9d5b235222fdfd3409136772d682c94a
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-12 11:21:01 +0200

    Remove generated index.js from repo

--- .projenrc.js
+++ .projenrc.js
@@ -18,5 +18,8 @@ const project = new GemeenteNijmegenCdkApp({
   /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
+  gitignore: [
+    'src/LogLambda/index.js'
+  ]
 });
 project.synth();
