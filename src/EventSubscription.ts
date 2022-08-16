08a7ca756b1222e77d8ef9b10f4af1e7231c6325
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-16 11:51:13 +0200

    Add ECS task state event monitoring - refactor event rules/subscriptions

---
+++ src/EventSubscription.ts
@@ -0,0 +1,32 @@
+import { aws_events_targets, aws_kms, aws_sns } from 'aws-cdk-lib';
+import { EventPattern, Rule } from 'aws-cdk-lib/aws-events';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+interface EventSubscriptionProps {
+  topic: aws_sns.Topic;
+  topicKey: aws_kms.Key;
+  pattern: EventPattern;
+  ruleDescription: string;
+}
+export class EventSubscription extends Construct {
+  /**
+   * Add an eventbridge rule for an event pattern, and
+   * subscribe the provided SNS topic to this rule. Also
+   * grants eventbridge publish permissions for this rule.
+   */
+  constructor(scope: Construct, id: string, props: EventSubscriptionProps) {
+    super(scope, id);
+
+    const rule = new Rule(this, `${id}-rule`, {
+      description: props.ruleDescription,
+      targets: [new aws_events_targets.SnsTopic(props.topic)],
+      eventPattern: props.pattern,
+    });
+
+    const principal = new ServicePrincipal('events.amazonaws.com', {
+      conditions: { StringEquals: { 'sns:Endpoint': rule.ruleArn } },
+    });
+    props.topic.grantPublish(principal);
+  }
+}
