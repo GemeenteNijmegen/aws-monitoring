853d3bca5bc316e93cd6d4e805a5eec1bf9c26f3
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-25 11:05:35 +0200

    Rename alarm, add description

--- src/AssumedRoleAlarms.ts
+++ src/AssumedRoleAlarms.ts
@@ -35,6 +35,8 @@ export class AssumedRoleAlarms extends Construct {
         }),
         evaluationPeriods: 1,
         threshold: 1,
+        alarmName: `Role ${role} was assumed`,
+        alarmDescription: `This alarm triggers if the role ${role} is assumed by any user. This role has more priviliges than normal use require, so when it is assumed it's use should be logged and explained.`
       });
       alarm.applyRemovalPolicy(RemovalPolicy.DESTROY);
     });
