b2e6552f9a6c9bb581f5473251402069ac6227cc
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-08-30 15:16:39 +0200

    feat: Monitor concurrent executions over all lambdas in region

---
+++ src/DefaultAlarms.ts
@@ -0,0 +1,35 @@
+import { Duration } from 'aws-cdk-lib';
+import { Alarm, Metric } from 'aws-cdk-lib/aws-cloudwatch';
+import { Construct } from 'constructs';
+
+
+export class DefaultAlarms extends Construct {
+  /**
+   * Setup alarms for metrics we want to monitor in each account.
+   */
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.addConcurrentExecutionsAlarm();
+  }
+
+  /**
+   * Add an alarm for concurrent executions.
+   *
+   * Alarm is set to alarm at 20 concurrent
+   * executions over all lambdas in the region.
+   */
+  private addConcurrentExecutionsAlarm() {
+    const metric = new Metric({
+      metricName: 'ConcurrentExecutions',
+      namespace: 'AWS/Lambda',
+      statistic: 'Maximum',
+      period: Duration.minutes(5),
+    });
+    new Alarm(this, 'concurrent-executions', {
+      metric: metric,
+      evaluationPeriods: 1,
+      threshold: 20,
+    });
+  }
+}
