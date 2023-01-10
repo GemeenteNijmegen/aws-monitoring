959f17aa3896278adcf5f7f2db89f0673f7c377a
Marnix Dessing <m.dessing@nijmegen.nl>
2023-01-10 16:35:30 +0100

    fix: section per log

--- src/LogSubscriptionsLambda/MessageFormatter.ts
+++ src/LogSubscriptionsLambda/MessageFormatter.ts
@@ -1,89 +1,32 @@
-import fs from 'fs';
-import path from 'path';
 import { CloudWatchLogsDecodedData } from 'aws-lambda';
 
 const codeBlock = '```';
 
-interface MessageParameters {
-  title: string;
-  message: string;
-  context: {
-    type: string;
-    account: string;
-  };
-}
+export class LogsMessageFormatter {
 
-export class MessageFormatter<T> {
-  message: T;
+  message: CloudWatchLogsDecodedData;
   account: string;
-  constructor(message: T, account: string) {
+
+  constructor(message: CloudWatchLogsDecodedData, account: string) {
     this.message = message;
     this.account = account;
   }
 
-  /**
-   * Do some cleanup / validation of the MessageParameters object.
-   *
-   * @param parameters {Messageparameters} the filled params
-   * @returns {Messageparameters} the cleaned params
-   */
-  cleanParameters(parameters: MessageParameters): MessageParameters {
-    // Slack header block allows text to be max 151 chars long.
-    const maxHeaderLength = 151;
-    parameters.title = parameters.title.substring(0, maxHeaderLength - 1);
-    return parameters;
-  }
-
   formattedMessage(): any {
-    const parameters = this.cleanParameters(this.messageParameters());
-    const templateBuffer = fs.readFileSync(path.join(__dirname, 'template.json'));
-    const templateString = templateBuffer.toString();
-    let blockString = templateString.replace('<HEADER>', parameters.title);
-    blockString = blockString.replace('<CONTEXT_ACCOUNT>', parameters.context.account);
-    blockString = blockString.replace('<CONTEXT_TYPE>', parameters.context.type);
-    blockString = blockString.replace('<MESSAGE>', parameters.message);
 
-    try {
-      const message = JSON.parse(blockString);
-      return message;
-    } catch (error: any) {
-      console.debug(error);
-      console.debug(blockString);
-    }
-  }
-
-  messageParameters(): MessageParameters {
-    return {
-      title: '',
-      message: '',
-      context: {
-        type: '',
-        account: this.account,
-      },
-    };
-  }
-}
-
-
-export class LogsMessageFormatter extends MessageFormatter<CloudWatchLogsDecodedData> {
-
-  messageParameters(): MessageParameters {
-    let messageObject = {
-      title: 'Log subscription',
-      message: `*Log group:* ${this.message.logGroup} `,
-      context: {
-        type: 'logs',
-        account: this.account,
-      },
-    };
-
-    messageObject.message += codeBlock;
-    this.message.logEvents.forEach(log => {
-      messageObject.message += this.escapeJson(log.message + '\n');
+    const message = new SlackMessage();
+    message.addHeader("Log subscription");
+    message.addContext({
+      account: this.account,
+      "log group": this.message.logGroup,
     });
-    messageObject.message += codeBlock;
 
-    return messageObject;
+    this.message.logEvents.forEach(log => {
+      const text = `${codeBlock}${this.escapeJson(log.message)}${codeBlock}`;
+      message.addSection(text);
+    });
+
+    return message.getSlackMessage();
   }
 
   private escapeJson(json: string) {
@@ -95,6 +38,56 @@ export class LogsMessageFormatter extends MessageFormatter<CloudWatchLogsDecoded
       .replace(/\t/g, '\\t')
       .replace(/\\b/g, '\\b')
       .replace(/\f/g, '\\f');
+  }
+
+}
+
+class SlackMessage {
+
+  private blocks: any[] = [];
+
+  addHeader(text: string) {
+    this.blocks.push({
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": text,
+        "emoji": true
+      }
+    });
+  }
+
+  addContext(context: { [name: string]: string }) {
+    const elements = Object.entries(context).map((entry) => {
+      return {
+        "type": "mrkdwn",
+        "text": `${entry[0]}: *${entry[1]}*`,
+      }
+    });
+    this.blocks.push({
+      type: "context",
+      elements: elements
+    });
+  }
+
+  addSection(text: string){
+    if(!text || text.length > 3000){
+      console.log("Message is empty or too long, max 3000", text);
+      text = '(message ommited)'
+    }
+    this.blocks.push({
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": text,
+      }
+    });
+  }
+
+  getSlackMessage(){
+    return {
+      blocks: this.blocks,
+    }
   }
 
 }
