0f315ba95aed4b7bd4648448d07adffc8d9efedd
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-01-19 13:44:26 +0100

    feat: Exclude securityhub role error
    Exclude error for SNS dummy topic

--- src/LogLambda/LogsEventHandler.ts
+++ src/LogLambda/LogsEventHandler.ts
@@ -7,6 +7,7 @@ import { getAccount, stringMatchesPatternInArray } from './utils';
 
 const excludedMessageStrings = [
   'assumed-role/config-drift-detection-role/configLambdaExecution is not authorized to perform: iam:GetRole on resource',
+  'AWSServiceRoleForSecurityHub/securityhub is not authorized to perform: SNS:ListSubscriptionsByTopic on resource: .*:CWAlarmDummyTopic',
 ];
 
 
