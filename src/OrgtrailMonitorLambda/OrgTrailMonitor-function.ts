2347c7dae49ae2cd36b410908ae9e4a6422bdd42
Marnix Dessing <m.dessing@nijmegen.nl>
2024-01-04 15:42:57 +0100

    feat: upgrade projen-project-type (stuck on old version)

--- src/OrgtrailMonitorLambda/OrgTrailMonitor-function.ts
+++ src/OrgtrailMonitorLambda/OrgTrailMonitor-function.ts
@@ -17,7 +17,7 @@ export class OrgTrailMonitorFunction extends lambda.Function {
     super(scope, id, {
       description: 'src/OrgtrailMonitorLambda/OrgTrailMonitor.lambda.ts',
       ...props,
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: new lambda.Runtime('nodejs18.x', lambda.RuntimeFamily.NODEJS),
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../assets/OrgtrailMonitorLambda/OrgTrailMonitor.lambda')),
     });
