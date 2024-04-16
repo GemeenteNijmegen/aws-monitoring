ae0bb3fdd3b0eac5251ab15fb9be6b77f1526679
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-04-16 17:02:03 +0200

    fix: Explicitly set admin role

--- src/MonitoringTargetStage.ts
+++ src/MonitoringTargetStage.ts
@@ -1,7 +1,8 @@
 import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
 import { Aspects, Stack, Stage, StageProps, Tags } from 'aws-cdk-lib';
 import { EventPattern } from 'aws-cdk-lib/aws-events';
-import { StackSet, StackSetTarget, StackSetTemplate } from 'cdk-stacksets';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import { DeploymentType, StackSet, StackSetTarget, StackSetTemplate } from 'cdk-stacksets';
 import { Construct } from 'constructs';
 import { AggregatorStack } from './AggregatorStack';
 import { DeploymentEnvironment } from './DeploymentEnvironments';
@@ -39,12 +40,15 @@ export class MonitoringTargetStage extends Stage {
     Tags.of(this).add('Project', Statics.projectName);
     Aspects.of(this).add(new PermissionsBoundaryAspect());
 
-    const stack = new Stack(this, 'stacksetemptystack');
+    const stack = new Stack(this, 'eventbridgeforwarder');
     const eventbridgeForwarderStack = new EventbridgeForwarderStack(stack, 'eventbridge-forwarder', { targetRegion: 'eu-central-1' });
     new StackSet(stack, 'StackSet', {
       target: StackSetTarget.fromAccounts({
         regions: ['us-east-1', 'eu-west-1'],
         accounts: props.deployToEnvironments.map(environment => environment.env.account).filter(account => account) as string[],
+      }),
+      deploymentType: DeploymentType.selfManaged({
+        adminRole: Role.fromRoleName(this, 'cdkrole', 'cdk-hnb659fds-cfn-exec-role-836443378780-eu-central-1'),
       }),
       template: StackSetTemplate.fromStackSetStack(eventbridgeForwarderStack),
     });
