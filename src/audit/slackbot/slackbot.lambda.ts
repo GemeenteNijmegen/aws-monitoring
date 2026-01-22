50b76d0f06a8f5fc6d3b559068c64d54585bc5d3
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-22 16:33:21 +0100

    chore: nice error handling

--- src/audit/slackbot/slackbot.lambda.ts
+++ src/audit/slackbot/slackbot.lambda.ts
@@ -5,37 +5,44 @@ import { SlackThreadResponse } from '../shared/SlackThreadResponse';
 import { TrackedSlackMessageRepository } from '../shared/TrackedSlackMessageRepository';
 import { slackAuthenticate } from './slack-authenticate';
 import { TrackedSlackMessageParser } from './TrackedSlackMessageParser';
-import { error } from 'console';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
+  // Setup
+  const secret = await getSlackSecret();
   const slackThreadResponse = new SlackThreadResponse(await getSlackBotToken());
   const repository = new TrackedSlackMessageRepository(process.env.MESSAGE_TABLE_NAME!);
 
-
   let trackedSlackMessage: TrackedSlackMessage | undefined = undefined;
   try {
-    const secret = await getSlackSecret();
+
+    // Parse body
+    const body = JSON.parse(event.body ?? '{}');
+
+    // Authenticate
     const authenticated = await slackAuthenticate(event, secret);
     if (!authenticated) {
       console.log('Unauthorized!');
-      return response(403);
+      return response(body, 403);
     }
     console.log('Authenticated!');
 
+    // Parse message
     trackedSlackMessage = TrackedSlackMessageParser.parse(event);
 
+    // Store message in message table
     await repository.save(trackedSlackMessage);
     console.log(`Successfully saved message (${trackedSlackMessage.messageId}) to DynamoDB for ${trackedSlackMessage.trackingGoal} registration purposes`);
 
     // Send response
     await slackThreadResponse.send(trackedSlackMessage.channelId, trackedSlackMessage.threadId, `✅ - DevOps bot is following this thread for ${trackedSlackMessage.trackingGoal} registration purposes`);
+    return response(body);
 
-    return response();
   } catch (error) {
     console.error('Error processing message:', error);
 
+    // send a nice error if possible
     if (trackedSlackMessage?.threadId && error instanceof Error) {
       await slackThreadResponse.send(trackedSlackMessage.channelId, trackedSlackMessage.threadId, `❗️ - Failed to archive this thread. ${error.message} `);
     }
@@ -69,9 +76,16 @@ async function getSlackBotToken() {
 }
 
 
-function response(statusCode: number = 200) {
+function response(body: any, statusCode: number = 200) {
+  // Handle Slack URL verification challenge
+  if (body.type === 'url_verification') {
+    return {
+      statusCode: statusCode,
+      body: JSON.stringify({ challenge: body.challenge }),
+    };
+  }
   return {
     statusCode: statusCode,
-    body: '',
-  };
+    body: ''
+  }
 }
