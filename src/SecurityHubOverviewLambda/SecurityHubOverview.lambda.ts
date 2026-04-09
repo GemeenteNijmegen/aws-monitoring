dcaaaa97069f0aa8388ce88d47aa3a3a9f16e7fd
Marnix Dessing <m.dessing@nijmegen.nl>
2026-04-09 10:49:25 +0200

    chore: add dashes in front of findings in slack message

--- src/SecurityHubOverviewLambda/SecurityHubOverview.lambda.ts
+++ src/SecurityHubOverviewLambda/SecurityHubOverview.lambda.ts
@@ -1,7 +1,7 @@
 import { AwsSecurityFinding, SecurityHubClient } from '@aws-sdk/client-securityhub';
 import { deploymentEnvironments } from '../DeploymentEnvironments';
-import { SecurityHubService } from './SecurityHubService';
 import { SlackMessage } from '../monitoringLambda/SlackMessage';
+import { SecurityHubService } from './SecurityHubService';
 
 const MAX_FINDINGS_PER_CRITICALITY = 50;
 
@@ -61,7 +61,7 @@ function addFindingsSection(message: SlackMessage, header: string, findings: Aws
   if (otherFindings.length > 0) {
     otherFindings.splice(0, MAX_FINDINGS_PER_CRITICALITY).forEach(finding => {
       const accountName = lookupAccountName(finding.AwsAccountId);
-      otherFindingsMessage.push(`${finding.Title} (${accountName}, ${finding.ProductName ?? 'unknown product'})`);
+      otherFindingsMessage.push(`- ${finding.Title} (${accountName}, ${finding.ProductName ?? 'unknown product'})`);
     });
 
     if (otherFindings.length > MAX_FINDINGS_PER_CRITICALITY) {
